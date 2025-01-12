1035529c135b552e38b5e61efab841889298ec31
zarafarooqui <engrimsha@gmail.com>
2025-01-12 14:41:58 +0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,24 @@
+      #Tom
+class robot:
+ grade=10 
+ name="Tom"
+ def introduction(self):
+  print("Hi I am a robot ")
+ def details(self):
+  print("My name is",self.name)
+  print("I study in grade",self.grade)
+ob= robot()
+ob.introduction()
+ob.details()
+       #Jerry
+class robot:
+ grade=11 
+ name="Jerry"
+ def introduction(self):
+  print("Hi I am a robot ")
+ def details(self):
+  print("My name is",self.name)
+  print("I study in grade",self.grade)
+ob= robot()
+ob.introduction()
+ob.details()
